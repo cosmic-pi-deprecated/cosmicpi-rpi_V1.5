c320032c2fdb812531b19b1f9c2735f4349bbc77
James Devine <devine.jd@gmail.com>
2018-01-29 22:01:11 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Sorry. This procedure isn't ready yet, We're working on it! "
 
+echo "--- Part 1: Expand the file system ---"
 echo "--- Expanding root file system ---"
 sudo raspi-config --expand-rootfs
 cp -f rebootrc.local /etc/rc.local
+chmod +x partialinstall.sh
+
+echo "--- Finished setup part 1! Rebooting now for part 2 ---"
+sleep 10
+sudo reboot now
