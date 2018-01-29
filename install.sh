4e0af756dbae8dc9f3e4597e672b12ee4a6e027d
James Devine <devine.jd@gmail.com>
2018-01-29 22:31:07 +0100

    added chmod + exec line

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "Sorry. This procedure isn't ready yet, We're working on it! "
 
 echo "--- Part 1: Expand the file system ---"
 echo "--- Expanding root file system ---"
+chmod +x rebootrc.local
 sudo raspi-config --expand-rootfs
 cp -f rebootrc.local /etc/rc.local
 chmod +x partialinstall.sh
