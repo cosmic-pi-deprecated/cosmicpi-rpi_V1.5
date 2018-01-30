f475a78b45e6ac03eb3c21b7c5d631f42934bee2
James Devine <devine.jd@gmail.com>
2018-01-30 22:33:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ echo "Sorry. This procedure isn't ready yet, We're working on it! "
 
 echo "--- Part 1: Expand the file system ---"
 echo "--- Expanding root file system ---"
-chmod +x rebootrc.local
+#chmod +x rebootrc.local
 sudo raspi-config --expand-rootfs
-cp -f rebootrc.local /etc/rc.local
+#cp -f rebootrc.local /etc/rc.local
 chmod +x partialinstall.sh
 
-echo "--- Finished setup part 1! Rebooting now for part 2 ---"
+echo "--- Finished setup part 1! Rebooting, log back in and run sudo ./partialinstall.sh for part 2 ---"
 sleep 10
 sudo reboot now
