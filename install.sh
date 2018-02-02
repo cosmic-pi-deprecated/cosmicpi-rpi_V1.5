1ed7b7fa90f7f2d30d8704f6179eb648fb86e07b
James Devine <devine.jd@gmail.com>
2018-02-02 20:56:03 +0100

    consistency with name change for second part installation script

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ echo "--- Expanding root file system ---"
 #chmod +x rebootrc.local
 sudo raspi-config --expand-rootfs
 #cp -f rebootrc.local /etc/rc.local
-chmod +x partialinstall.sh
+chmod +x installparttwo.sh
 
-echo "--- Finished setup part 1! Rebooting, log back in and run sudo ./partialinstall.sh for part 2 ---"
+echo "--- Finished setup part 1! Rebooting, log back in and run sudo ./installparttwo.sh for part 2 ---"
 sleep 10
 sudo reboot now
