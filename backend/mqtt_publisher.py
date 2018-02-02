da8466e979e30e49f05b44e3a279098646ed75a4
HenniOVP <hendrikborras@web.de>
2018-02-02 20:36:18 +0100

    Added import

--- backend/mqtt_publisher.py
+++ backend/mqtt_publisher.py
@@ -12,6 +12,7 @@ import random
 import json
 import subprocess
 import socket
+import struct
 
 import logging as log
 log.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=log.INFO)
