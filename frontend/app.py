b6e73fa76da56595a119f8fe346b07d3fd631ad2
Darko Lukic <lukicdarkoo@gmail.com>
2018-03-21 21:42:19 +0000

    Add CORS

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api
+from flask_cors import CORS
 from .params import Params
 from .wifi import Wifi
 from .histogram import Histogram
@@ -8,10 +9,12 @@ from .data import Data
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Params, '/params')
-api.add_resource(Wifi, '/wifi')
-api.add_resource(Histogram, '/histogram.png')
-api.add_resource(Data, '/data')
+CORS(app, resources=r'/api/*')
+
+api.add_resource(Params, '/api/params')
+api.add_resource(Wifi, '/api/wifi')
+api.add_resource(Histogram, '/api/histogram.png')
+api.add_resource(Data, '/api/data')
 
 if __name__ == '__main__':
     app.run(debug=True)
